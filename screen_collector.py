f5fab6b9349452d9a127cfee72470cc607b10f13
lothar1998 <33781380+lothar1998@users.noreply.github.com>
2020-07-03 18:50:34 +0200

    [BD-59] performance boost, new delay computation based on distance between actual and previous point

--- screen_collector.py
+++ screen_collector.py
@@ -4,28 +4,49 @@ from pynput.keyboard import Listener as KeyboardListener
 from pynput import keyboard
 from pynput import mouse
 from random import uniform
+from pyautogui import *
 from file_saver import *
-from math import floor
+from math import floor, sqrt
 
 
 class ScreenCollector:
+    p_prev = position()
+
     @staticmethod
     def run(filename, sequence_name, sequence_id):
         mouse_controller = Controller()
 
-        def random_delay():
-            return uniform(2, 3)
+        def compute_delay(p: (int, int), p_prev: (int, int)):
+
+            min_deviation = 0.9
+            max_deviation = 1.0
+            bias = 0.4
+
+            def distance(p1: (int, int), p2: (int, int)):
+                print(p1, p2)
+                return sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
+
+            print(distance(p, p_prev))
+            return distance(p, p_prev) * uniform(min_deviation, max_deviation) * 1e-3 + bias
 
         events = []
 
         def on_click(x, y, button, pressed):
+
+            p = (floor(x), floor(y))
+
             if pressed:
                 if button == mouse.Button.left:
-                    events.append(Event(floor(x), floor(y), 0, 0, random_delay(), EventType.BUTTON_LEFT.value))
+                    events.append(Event(p[0], p[1], 0, 0, compute_delay(p, ScreenCollector.p_prev), EventType.BUTTON_LEFT.value))
                 elif button == mouse.Button.right:
-                    events.append(Event(floor(x), floor(y), 0, 0, random_delay(), EventType.BUTTON_RIGHT.value))
+                    events.append(Event(p[0], p[1], 0, 0, compute_delay(p, ScreenCollector.p_prev), EventType.BUTTON_RIGHT.value))
+
+            ScreenCollector.p_prev = p
 
         def on_scroll(x, y, dx, dy):
+
+            p = (floor(x), floor(y))
+
             if len(events) > 0:
                 previous = events[len(events) - 1]
 
@@ -33,13 +54,20 @@ class ScreenCollector:
                     previous.dx += dx
                     previous.dy += dy
                 else:
-                    events.append(Event(floor(x), floor(y), dx, dy, random_delay(), EventType.SCROLL.value))
+                    events.append(Event(p[0], p[1], dx, dy, compute_delay(p, ScreenCollector.p_prev), EventType.SCROLL.value))
 
             else:
-                events.append(Event(floor(x), floor(y), dx, dy, random_delay(), EventType.SCROLL.value))
+                events.append(Event(p[0], p[1], dx, dy, compute_delay(p, ScreenCollector.p_prev), EventType.SCROLL.value))
+
+            ScreenCollector.p_prev = p
 
         def on_space_record_move(x, y):
-            events.append(Event(floor(x), floor(y), 0, 0, random_delay(), EventType.MOVE.value))
+
+            p = (floor(x), floor(y))
+
+            events.append(Event(p[0], p[1], 0, 0, compute_delay(p, ScreenCollector.p_prev), EventType.MOVE.value))
+
+            ScreenCollector.p_prev = p
 
         with MouseListener(on_click=on_click, on_scroll=on_scroll) as mouse_listener:
             def on_keyboard_press(key):
