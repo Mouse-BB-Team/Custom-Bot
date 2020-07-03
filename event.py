4a0e474c6db178ecee1d854b79dfca4040e4b928
lothar1998 <33781380+lothar1998@users.noreply.github.com>
2020-07-03 19:06:23 +0200

    [BD-62] support events ordering based on yaml ord field

--- event.py
+++ event.py
@@ -9,10 +9,11 @@ class EventType(Enum):
 
 
 class Event:
-    def __init__(self, x, y, dx, dy, delay, event):
+    def __init__(self, x, y, dx, dy, delay, event, order=0):
         self.x = x
         self.y = y
         self.delay = delay
         self.event = event
         self.dx = dx
         self.dy = dy
+        self.order = order
